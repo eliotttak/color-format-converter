0fe0758d913b23f9378533360011708a1e3e921c
Eliott Takvorian <98952935+eliotttak@users.noreply.github.com>
2024-11-24 18:31:01 +0100

    Create the Python files
    main.py and main.min.py

---
+++ RGB-to-HSL/main.py
@@ -0,0 +1,77 @@
+def rgb_to_hsl(r, g, b, maximums):
+    """
+    The function rgb_to_hsl() convert RGB colors in HSL. You can choose the value ranges in the fourth argument.
+    How to use :
+    rgb_to_hsl(r: number, g: number, b: number, {
+        rgb: {
+            r: number = 255,
+            g: number = 255,
+            b: number = 255
+        },
+        hsl: {
+            h: number = 360,
+            s: number = 100,
+            l: number = 100
+        
+    })
+    """
+    maximums["hsl"] = getattr(maximums, 'hsl', {
+        "h": 360,
+        "s": 100,
+        "l": 100
+    })
+    
+    maximums["rgb"] = getattr(maximums, 'rgb', {
+        "r": 255,
+        "g": 255,
+        "b": 255
+    })
+    
+    maximums["hsl"]["h"] = getattr(maximums["hsl"], "h", 360)
+    maximums["hsl"]["s"] = getattr(maximums["hsl"], "s", 100)
+    maximums["hsl"]["l"] = getattr(maximums["hsl"], "l", 100)
+    maximums["rgb"]["r"] = getattr(maximums["rgb"], "r", 255)
+    maximums["rgb"]["g"] = getattr(maximums["rgb"], "g", 255)
+    maximums["rgb"]["b"] = getattr(maximums["rgb"], "b", 255)
+
+    r_ = r / maximums["rgb"]["r"]
+    g_ = g / maximums["rgb"]["g"]
+    b_ = b / maximums["rgb"]["b"]
+
+    if r_ < 0 or r_ > 1 or g_ < 0 or g_ > 1 or b_ < 0 or b_ > 1 :
+        return None
+    
+    c_max = max(r_, g_, b_)
+    c_min = min(r_, g_, b_)
+    delta = c_max - c_min
+    
+
+    h_ = None, s_ = None, l_ = None
+    if delta == 0 :
+        h_ = 0
+    
+    elif c_max == r_ :
+        h_ = 60 * ((g_ - b_) / delta) % 6
+    
+    elif c_max == g_ :
+        h_ = 60 * ((b_ - r_) / delta) + 2
+    
+    elif c_max == b_ :
+        h_ = 60 * ((r_ - g_) / delta) + 4
+    
+
+    if delta == 0 :
+        s_ = 0
+    
+    else :
+        s_ = delta / (1 - abs(2 * l_ - 1))
+    
+    l_ = (c_max + c_min) / 2
+    
+    return {
+        "h": (h_ / 360) * maximums["hsl"]["h"],
+        "s": s_ * maximums["hsl"]["s"],
+        "l": l_ * maximums["hsl"]["l"]
+    }
+    
+    
