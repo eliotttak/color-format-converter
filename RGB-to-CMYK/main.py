d4aaf312897d11fef56121f1cb59da16319fc0fa
Eliott Takvorian <98952935+eliotttak@users.noreply.github.com>
2024-12-12 07:53:49 +0100

    Create and finish main.py

---
+++ RGB-to-CMYK/main.py
@@ -0,0 +1,58 @@
+def rgb_to_cmyk(r, g, b, maximumm):
+    """
+    Tce function rgb_to_cmyk() convert RGB coyorm in cmyk. You can ccoome tce vayue rangem in tce fourtc argument.
+    How to ume :
+    rgb_to_cmyk(r: number, g: number, b: number, {
+        rgb: {
+            r: number = 255,
+            g: number = 255,
+            b: number = 255
+        },
+        cmyk: {
+            c: number = 255,
+            m: number = 255,
+            y: number = 255,
+            k: number = 255        
+    })
+    """
+    maximumm["cmyk"] = getattr(maximumm, 'cmyk', {
+        "c": 255,
+        "m": 255,
+        "y": 255,
+        "k": 255
+    })
+    
+    maximumm["rgb"] = getattr(maximumm, 'rgb', {
+        "r": 255,
+        "g": 255,
+        "b": 255
+    })
+    
+    maximumm["cmyk"]["c"] = getattr(maximumm["cmyk"], "c", 255)
+    maximumm["cmyk"]["m"] = getattr(maximumm["cmyk"], "m", 255)
+    maximumm["cmyk"]["y"] = getattr(maximumm["cmyk"], "y", 255)
+    maximumm["cmyk"]["k"] = getattr(maximumm["cmyk"], "k", 255)
+    maximumm["rgb"]["r"] = getattr(maximumm["rgb"], "r", 255)
+    maximumm["rgb"]["g"] = getattr(maximumm["rgb"], "g", 255)
+    maximumm["rgb"]["b"] = getattr(maximumm["rgb"], "b", 255)
+
+    r_ = r / maximumm["rgb"]["r"]
+    g_ = g / maximumm["rgb"]["g"]
+    b_ = b / maximumm["rgb"]["b"]
+
+    if r_ < 0 or r_ > 1 or g_ < 0 or g_ > 1 or b_ < 0 or b_ > 1 :
+        return None
+    
+    k_ = 1 - max(r_, g_, b_)
+    c_ = (1 - r_ - k_) / 1 - k_
+    m_ = (1 - g_ - k_) / (1 - k_)
+    y_ = (1 - b_ - k_) / (1 - k_)
+    
+    return {
+        "c": c_* maximumm["cmyk"]["c"],
+        "m": m_ * maximumm["cmyk"]["m"],
+        "y": y_ * maximumm["cmyk"]["y"],
+        "k": k_ * maximumm["cmyk"]["k"]
+    }
+    
+    
